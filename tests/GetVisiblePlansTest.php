2b152fb773a54a8efe3170bd935b976e28413874
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-04-10 13:55:27 +0200

    Linting

--- tests/GetVisiblePlansTest.php
+++ tests/GetVisiblePlansTest.php
@@ -2,10 +2,7 @@
 
 use ArtisanBuild\Till\Actions\GetVisiblePlans;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Config;
 
 it('hides the free plan by default', function (): void {
     expect(app(GetVisiblePlans::class)())->toBeInstanceOf(Collection::class)->toHaveCount(3);
 });
-
-
